71634f886660b63b6ff977906114ff88c945b288
thiphan <thoaptk07@gmail.com>
2019-03-05 20:16:10 +0000

    add CRUD pages

--- lunasList/app/controllers/events_controller.rb
+++ lunasList/app/controllers/events_controller.rb
@@ -7,5 +7,12 @@ class EventsController < ApplicationController
   end
 
   def new
+      redirect_to index_event_url(@event), alert: "Error: event not found"
+end
+  def destroy
+    @event = event.find(params[:id])
+    @event.destroy
+    redirect_to events_url, notice: 'Event was successfully destroyed'
   end
+  
 end
