60aa65b28d47a5f17492c4ce4871422283eba1cf
thiphan <thoaptk07@gmail.com>
2019-03-05 20:21:07 +0000

    add CRUD pages

--- lunasList/app/controllers/events_controller.rb
+++ lunasList/app/controllers/events_controller.rb
@@ -3,14 +3,45 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
-  def edit
+  def show
+    begin
+      @event = Event.find(params[:id])
+    rescue
+      redirect_to event_url(@event), alert: "Error: event not found"
+    end
+
   end
 
   def new
-      redirect_to index_event_url(@event), alert: "Error: event not found"
-end
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(params.require(:event).permit(:description, :location, :time))
+    if @event.save
+      redirect_to event_url(@event), notice: 'Event was successfully created'
+    else
+      flash.now[:alert] = 'Error! Unable to create new event'
+      render :new
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(params.require(:event).permit(:description, :location, :time))
+      redirect_to event_url(@event), notice: 'Event was successfully updated'
+    else
+      flash.now[:alert] = 'Error! Unable to update event'
+      render :edit
+    end
+  end
+
   def destroy
-    @event = event.find(params[:id])
+    @event = Event.find(params[:id])
     @event.destroy
     redirect_to events_url, notice: 'Event was successfully destroyed'
   end
--- lunasList/config/routes.rb
+++ lunasList/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'events/index'
-  get 'events/edit'
-  get 'events/new'
+  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -9,4 +7,18 @@ Rails.application.routes.draw do
     root to: 'pages#home'
 
     get 'home', to: 'pages#home', as: 'home'
+
+    #------- EVENT ------------
+    get 'events', to: 'events#index', as: 'events'
+    get 'events/edit', to: 'events#edit'
+    post 'events', to: 'events#create'
+    get 'events/new', to: 'events#new', as: 'new_event'
+    get 'events/:id', to: 'events#show', as: 'event'
+    get 'events/:id/edit', to: 'events#edit', as: 'edit_event'
+    patch 'events/:id', to: 'events#update'
+    put 'events/:id', to: 'events#update'
+    delete 'events/:id', to: 'events#destroy'
+
+
+    
 end
