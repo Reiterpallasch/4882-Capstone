7d3bd1e8b80377b3b8125e0a999df6c64de423f3
thiphan <thoaptk07@gmail.com>
2019-04-03 22:45:28 +0000

    user-pet association

--- lunasList/app/models/pet.rb
+++ lunasList/app/models/pet.rb
@@ -9,7 +9,13 @@
 #  type       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_pets_on_user_id  (user_id)
 #
 
 class Pet < ApplicationRecord
+    belong_to :user, class_name: 'User', foreign_key: 'user_id', inverse_of: :pets, optional: true
 end
--- lunasList/db/schema.rb
+++ lunasList/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_230345) do
+ActiveRecord::Schema.define(version: 2019_04_03_221751) do
 
   create_table "events", force: :cascade do |t|
     t.string "location"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 2019_03_30_230345) do
     t.string "gender"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
