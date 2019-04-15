d2888bd5089a009d379209c953b701836f592a64
Reiterpallasch <monsterzero7@gmail.com>
2019-04-14 22:03:08 -0500

    added user information fields and added field to registration and edit pages

--- lunasList/app/controllers/application_controller.rb
+++ lunasList/app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
     protected
 
         def configure_permitted_parameters
-            added_attrs = [:email, :password, :admin, :image]
+            added_attrs = [:email, :password, :admin, :image, :name, :address, :city, :state, :zip, :phone]
             devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
             devise_parameter_sanitizer.permit :account_update, keys: added_attrs
 end
--- lunasList/db/schema.rb
+++ lunasList/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_191922) do
+ActiveRecord::Schema.define(version: 2019_04_15_025440) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -390,6 +390,12 @@ ActiveRecord::Schema.define(version: 2019_04_10_191922) do
     t.boolean "admin"
     t.boolean "business"
     t.string "businessName"
+    t.string "name"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
