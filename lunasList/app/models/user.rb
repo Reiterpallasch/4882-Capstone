a4337a833bc167cfae3296dfe54fabfece9887d1
Larz Leonard <32080758+LarzLeonard@users.noreply.github.com>
2019-04-15 21:55:13 -0500

    Update user.rb

--- lunasList/app/models/user.rb
+++ lunasList/app/models/user.rb
@@ -27,6 +27,8 @@
 #
 
 class User < ApplicationRecord
+  ratyrate_rater
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
