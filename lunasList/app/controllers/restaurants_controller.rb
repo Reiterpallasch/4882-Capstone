ba0714198ac6cf702340a322ac21039f6869fa16
LunasList <nhanks@memphis.edu>
2019-04-23 23:45:34 -0500

    Formatted Restaurant and Store pages

--- lunasList/app/controllers/restaurants_controller.rb
+++ lunasList/app/controllers/restaurants_controller.rb
@@ -15,7 +15,7 @@ class RestaurantsController < ApplicationController
 
     def create
 
-        @restaurant = Restaurant.new(params.require(:restaurant).permit(:name, :address, :city, :state, :style, :approved, :quality))
+        @restaurant = Restaurant.new(params.require(:restaurant).permit(:name, :address, :city, :state, :style, :approved, :quality, :additional))
  
         if @restaurant.save!
             redirect_to restaurants_url, notice: 'restaurant added'
@@ -36,7 +36,7 @@ class RestaurantsController < ApplicationController
         rescue
             redirect_to restaurants_url, alert: 'restaurant not found'
         end
-        if @restaurant.update(params.require(:restaurant).permit(:name, :address, :city, :state, :style, :approved, :quality))
+        if @restaurant.update(params.require(:restaurant).permit(:name, :address, :city, :state, :style, :approved, :quality, :additional))
             redirect_to restaurants_url, notice: 'restaurant updated'
         else
             flash.now[:alert] = 'update failed'
